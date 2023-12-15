f817e1f46b70fb2b03f5fdfa06ad463fe2159b43
Shubham Mishra <shubham.cmishra@gmail.com>
2023-12-15 09:59:18 -0800

    added BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import reportWebVitals from "./reportWebVitals";
+
 
 import BrowserRouter from 'react-router-dom/BrowserRouter'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <React.StrictMode>
     <App />
-    </BrowserRouter>,
+  </React.StrictMode>,
 )
+reportWebVitals();
