a21b9192ab4cd41098afe5d066419fac5d3b1e7e
Shubham Mishra <shubham.cmishra@gmail.com>
2023-12-14 18:03:19 -0800

    added BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>,
+    </BrowserRouter>,
 )
