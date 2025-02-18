9b4d5ac45e7fcadd140f79f99a0e438b27bce4e6
Shubham Mishra <shubham.cmishra@gmail.com>
2025-02-17 22:44:15 -0800

    Changes for skill section, still wip

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,8 +9,10 @@ function About() {
               <span className={styles.green}></span>
           </div>
           <div className={styles.content}>
-              <h2>About Me</h2>
-              <p>I am a Software Developer Engineer with 4+ years of experience in full-stack development, AI/ML, and cloud technologies. Passionate about building scalable applications.</p>
+              <h2>Shubham Mishra</h2>
+              <p>Software Developer Engineer with 4+ years of experience in developing innovative and scalable software solutions. Proven expertise in full-stack
+development. Adept at collaborating with cross-functional teams to deliver high-quality products. Seeking a SDE role to leverage my skills and passion
+for creating innovative solutions that enhance customer experiences and drive technological advancement.</p>
           </div>
       </section>
   );
