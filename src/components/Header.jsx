d8f33bb114f802650f1a1eb7932537ffa5f9d550
Shubham Mishra <shubham.cmishra@gmail.com>
2025-03-29 13:47:09 -0700

    removed comments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
-import { useEffect, useState } from 'react';
-import styles from '../style/Header.module.css';
-import { FaUser, FaCode, FaBriefcase, FaEnvelope, FaLaptop, FaEye } from 'react-icons/fa';
+import { useEffect, useState } from "react";
+import styles from "../style/Header.module.css";
+import {
+  FaUser,
+  FaCode,
+  FaBriefcase,
+  FaEnvelope,
+  FaLaptop,
+  FaEye,
+} from "react-icons/fa";
 import { TbFileText } from "react-icons/tb";
-
 function Header() {
   const [visitorCount, setVisitorCount] = useState(0);
 
   useEffect(() => {
-    const hasVisited = sessionStorage.getItem("hasVisited"); // Check if the session flag exists
+    const hasVisited = sessionStorage.getItem("hasVisited");
     if (!hasVisited) {
       let count = localStorage.getItem("visitorCount") || 0;
       count = parseInt(count) + 1;
       localStorage.setItem("visitorCount", count);
-      sessionStorage.setItem("hasVisited", "true"); // Set session flag to prevent double count
+      sessionStorage.setItem("hasVisited", "true");
       setVisitorCount(count);
     } else {
       setVisitorCount(localStorage.getItem("visitorCount") || 0);
@@ -22,12 +28,33 @@ function Header() {
   return (
     <nav className={styles.dock}>
       <div className={styles.dockIcons}>
-        <a href="/spidermines/ShubhamMishra_Resume.pdf" className={styles.dockItem}><TbFileText size={28} /><span>Resume</span></a>
-        <a href="#about" className={styles.dockItem}><FaUser size={28} /><span>About</span></a>
-        <a href="#skills" className={styles.dockItem}><FaCode size={28} /><span>Skills</span></a>
-        <a href="#projects" className={styles.dockItem}><FaBriefcase size={28} /><span>Projects</span></a>
-        <a href="#experience" className={styles.dockItem}><FaLaptop size={28} /><span>Experience</span></a>
-        <a href="#contact" className={styles.dockItem}><FaEnvelope size={28} /><span>Contact</span></a>
+        <a
+          href="/spidermines/ShubhamMishra_Resume.pdf"
+          className={styles.dockItem}
+        >
+          <TbFileText size={28} />
+          <span>Resume</span>
+        </a>
+        <a href="#about" className={styles.dockItem}>
+          <FaUser size={28} />
+          <span>About</span>
+        </a>
+        <a href="#skills" className={styles.dockItem}>
+          <FaCode size={28} />
+          <span>Skills</span>
+        </a>
+        <a href="#projects" className={styles.dockItem}>
+          <FaBriefcase size={28} />
+          <span>Projects</span>
+        </a>
+        <a href="#experience" className={styles.dockItem}>
+          <FaLaptop size={28} />
+          <span>Experience</span>
+        </a>
+        <a href="#contact" className={styles.dockItem}>
+          <FaEnvelope size={28} />
+          <span>Contact</span>
+        </a>
       </div>
       <div className={styles.dockBottom}>
         <span className={styles.visitorCount}>
